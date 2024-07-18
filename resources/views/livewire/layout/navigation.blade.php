d6c0415036be20b04b106701df72c83817ada13d
THEALIFHAKER1 <alifhaker1@gmail.com>
2024-07-19 07:32:52 +0800

    apply navigation design

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -16,95 +16,161 @@ new class extends Component
     }
 }; ?>
 
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}" wire:navigate>
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+
+<nav class="bg-white dark:bg-foreground/20 h-full w-[270px] flex-col lg:flex hidden ">
+    <div class="flex-grow gap-2 flex flex-col">
+        <div class="h-36 grid place-items-center gap-2 p-2">
+            <img class="h-24 aspect-square rounded-full" src="https://app-uat.lapasar.com/app/buyers/uploads/user/16995988830.JPG" alt="">
+
+            <ul class="w-full">
+                <li class="relative" x-data="{ open: false }">
+                    <button @click="open = !open" class="w-full flex items-center ">
+                        <div class="text-xs text-left">
+                            <div class="font-bold" x-data="{{ json_encode(['name' => auth()->user()->name]) }}" x-text="name" x-on:profile-updated.window="name = $event.detail.name"></div>
+                            <div>{{ auth()->user()->email }}</div>
+                        </div>
+                        <svg class="ml-auto h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                        </svg>
+                    </button>
+                    <ul x-show="open" class="w-full bg-white dark:bg-zinc-900 divide-y text-sm absolute shadow-md rounded-md">
+                        <li>
+                            <a href="/profile" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary ">
+                                <span class="icon">👤</span>
+                                <span class="ml-2">Profile</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" wire:click="logout" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary ">
+                                <span class="icon">🚪</span>
+                                <span class="ml-2">Logout</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="bg-background py-2 px-3 font-semibold w-full text-xs">Menu</div>
+        <div x-data="{ openMenu: null }" class="text-sm font-semibold">
+            <ul>
+                <li class="group">
+                    <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary ">
+                        <span class="icon">🏠</span>
+                        <span class="ml-2">Dashboard</span>
                     </a>
-                </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-            </div>
-
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div x-data="{{ json_encode(['name' => auth()->user()->name]) }}" x-text="name" x-on:profile-updated.window="name = $event.detail.name"></div>
-
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile')" wire:navigate>
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-
-                        <!-- Authentication -->
-                        <button wire:click="logout" class="w-full text-start">
-                            <x-dropdown-link>
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </button>
-                    </x-slot>
-                </x-dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
+                </li>
+                <li class="group">
+                    <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary ">
+                        <span class="icon">🧭</span>
+                        <span class="ml-2">Manage Location</span>
+                    </a>
+                </li>
+                <li x-data="{ id: 1 }">
+                    <a href="#" @click.prevent="openMenu = openMenu === id ? null : id" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">
+                        <span class="icon">👥</span>
+                        <span class="ml-2">Manage Users</span>
+                        <span class="ml-auto" x-show="openMenu !== id">+</span>
+                        <span class="ml-auto" x-show="openMenu === id">-</span>
+                    </a>
+                    <ul x-show="openMenu === id" class="pl-8 ">
+                        <li>
+                            <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">User Roles</a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">List of Users</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">
+                        <span class="icon">🔧</span>
+                        <span class="ml-2">Manage Workflow</span>
+                    </a>
+                </li>
+                <li x-data="{ id: 2 }">
+                    <a href="#" @click.prevent="openMenu = openMenu === id ? null : id" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">
+                        <span class="icon">📦</span>
+                        <span class="ml-2">Manage Orders</span>
+                        <span class="ml-auto" x-show="openMenu !== id">+</span>
+                        <span class="ml-auto" x-show="openMenu === id">-</span>
+                    </a>
+                    <ul x-show="openMenu === id" class="pl-8 ">
+                        <li>
+                            <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">Manage Orders</a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">Manage Pre-Orders</a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">Manage Budget Request</a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">Manage Cart ID</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">
+                        <span class="icon">🔔</span>
+                        <span class="ml-2">News (7)</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">
+                        <span class="icon">💬</span>
+                        <span class="ml-2">Chat</span>
+                    </a>
+                </li>
+                <li x-data="{ id: 3 }">
+                    <a href="#" @click.prevent="openMenu = openMenu === id ? null : id" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">
+                        <span class="icon">💰</span>
+                        <span class="ml-2">Manage Budget</span>
+                        <span class="ml-auto" x-show="openMenu !== id">+</span>
+                        <span class="ml-auto" x-show="openMenu === id">-</span>
+                    </a>
+                    <ul x-show="openMenu === id" class="pl-8 ">
+                        <li>
+                            <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">Manage Budget</a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">Manage GL CODE</a>
+                        </li>
+                    </ul>
+                </li>
+                <li x-data="{ id: 4 }">
+                    <a href="#" @click.prevent="openMenu = openMenu === id ? null : id" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">
+                        <span class="icon">⚖️</span>
+                        <span class="ml-2">Manage Users</span>
+                        <span class="ml-auto" x-show="openMenu !== id">+</span>
+                        <span class="ml-auto" x-show="openMenu === id">-</span>
+                    </a>
+                    <ul x-show="openMenu === id" class="pl-8 ">
+                        <li>
+                            <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">User to except</a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">Logs transaction</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">
+                        <span class="icon">📊</span>
+                        <span class="ml-2">Report</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center p-4 hover:bg-gray-200 hover:text-primary">
+                        <span class="icon">⚙️</span>
+                        <span class="ml-2">Customize Report</span>
+                    </a>
+                </li>
+            </ul>
         </div>
+
     </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800 dark:text-gray-200" x-data="{{ json_encode(['name' => auth()->user()->name]) }}" x-text="name" x-on:profile-updated.window="name = $event.detail.name"></div>
-                <div class="font-medium text-sm text-gray-500">{{ auth()->user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile')" wire:navigate>
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                <!-- Authentication -->
-                <button wire:click="logout" class="w-full text-start">
-                    <x-responsive-nav-link>
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </button>
-            </div>
-        </div>
+    <div class="mt-auto flex gap-2 border-t p-2">
+        <img class="w-5" src="https://app-uat.lapasar.com/app/buyers/assets2/images/lapasarfav.png" alt="">
+        <p class="text-xs">Copyright 2019 | Lapasar.com</p>
     </div>
 </nav>
