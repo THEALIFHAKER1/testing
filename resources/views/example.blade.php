cf5da69732e592b4e01e1da428642b884ab881d6
THEALIFHAKER1 <alifhaker1@gmail.com>
2024-07-19 06:25:40 +0800

    refactor location and add theme

--- resources/views/example.blade.php
+++ resources/views/example.blade.php
@@ -1,3 +1,16 @@
 <x-app-layout>
-    <livewire:counter />
+    <livewire:components.counter />
+    <livewire:components.themeswitcher />
+    <body class="bg-background text-foreground">
+        <div class="container mx-auto p-4">
+          <h1 class="text-2xl font-bold mb-4">Color Showcase</h1>
+          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div class="p-4 bg-primary text-primary-foreground rounded-md">Primary</div>
+            <div class="p-4 bg-secondary text-secondary-foreground rounded-md">Secondary</div>
+            <div class="p-4 bg-muted text-muted-foreground rounded-md">Muted</div>
+            <div class="p-4 bg-accent text-accent-foreground rounded-md">Accent</div>
+            <div class="p-4 bg-destructive text-destructive-foreground rounded-md">Destructive</div>
+          </div>
+        </div>
+      </body>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" x-data="{ darkMode: false }" x-bind:class="{'dark' : darkMode === true}"  x-init="
+    if (!('darkMode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        localStorage.setItem('darkMode', JSON.stringify(true));
+    }
+    darkMode = JSON.parse(localStorage.getItem('darkMode'));
+    $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,22 +19,11 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <body class="font-sans antialiased bg-background h-dvh overflow-auto">
+        <div class="flex h-full">
             <livewire:layout.navigation />
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
+            <main class="w-full h-full p-5 gap-5 flex flex-col">
+                {{ $slot  }}
             </main>
         </div>
     </body>
