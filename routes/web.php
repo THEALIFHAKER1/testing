c63373acb5408303545a91a3e4deb9980a42ba85
THEALIFHAKER1 <alifhaker1@gmail.com>
2024-07-19 05:51:54 +0800

    create an example page + components

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::view('/example', 'example')
+    ->middleware(['auth', 'verified'])
+    ->name('test');
+
 Route::view('/', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
